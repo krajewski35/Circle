eafd1b819b504a06675e28425386cd8b8d0d8dda
krajewski35 <krajew35@uwm.edu>
2023-10-25 08:00:37 -0500

    Update user_settings.php

--- pages/user_settings.php
+++ pages/user_settings.php
@@ -12,6 +12,10 @@ elseif ($_SESSION['member_type'] == 'admin') {
     header("Location: admin_page.php");
     exit();
 }
+//DEBUGGING
+if (!is_writable(session_save_path())) {
+    echo 'Session path "'.session_save_path().'" is not writable for PHP!'; 
+}
 
 //Include header
 $pagetitle = 'User Settings';
