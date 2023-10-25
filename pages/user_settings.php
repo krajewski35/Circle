a6a4904b29801f55f073c7d975d45cdcdbdd88e5
krajewski35 <krajew35@uwm.edu>
2023-10-25 08:11:09 -0500

    Update user_settings.php

--- pages/user_settings.php
+++ pages/user_settings.php
@@ -17,27 +17,13 @@ elseif ($_SESSION['member_type'] == 'admin') {
 $pagetitle = 'User Settings';
 include('includes/header.php');
 
-//Retrieve user info from cookies
-$user = array();
-//DEBUGGING
-foreach ($_SESSION as $key => $session_value) {
-    echo "$key: $session_value<br>";
-}
-$_SESSION['firstname'] = $user['firstname'];
-$_SESSION['lastname'] = $user['lastname'];
-$_SESSION['username'] = $user['username'];
-$_SESSION['email'] = $user['email'];
-$_SESSION['member_type'] = $user['member_type'];
-$_SESSION['member_purpose'] = $user['member_purpose'];
-$_SESSION['regdate'] = $user['regdate'];
-
 //Display content
-echo "<h3>{$user['firstname']} {$user['lastname']}</h3>";
-echo "<h4>{$user['username']}</h4>";
+echo "<h3>{$_SESSION['firstname']} {$_SESSION['lastname']}</h3>";
+echo "<h4>{$_SESSION['username']}</h4>";
 echo "<p class=\"userdetails\">" . 
-    "<b>Email address:</b>" . $user['email'] . "<br>
-    <b>User type:</b>" . ucfirst($user['member_type']) . "<br>
-    <b>User status:</b>" . ucfirst($user['member_purpose']) . "</p>";
+    "<b>Email address:</b>" . $_SESSION['email'] . "<br>
+    <b>User type:</b>" . ucfirst($_SESSION['member_type']) . "<br>
+    <b>User status:</b>" . ucfirst($_SESSION['member_purpose']) . "</p>";
 echo "<button type=\"button\" onclick=\"location.href='logout_page.php'\">Log out</button>";
 
 //Include footer
