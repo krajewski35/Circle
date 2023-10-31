f59cdb6ce2b272f10e33448502e18ab9bce59a6e
AichaAkbar <115574446+AichaAkbar@users.noreply.github.com>
2023-10-31 15:06:08 -0500

    Update index.php

--- circle/index.php
+++ circle/index.php
@@ -17,7 +17,7 @@ include("$homepath/includes/header.php");
 </div>
 <div class="box">
     <div class="box-text">
-        <h2 style= "font-family: Trebuchet MS, Helvetica, sans-serif">Circle is where volunteering comes in full circle!</h2>
+        <h2 style= "font-family: Trebuchet MS, Helvetica, sans-serif";  font-size = "15px";>Circle is where volunteering comes in full circle!</h2>
         <br><h2 style= "font-family: Trebuchet MS, Helvetica, sans-serif">• People • Causes • Rewards</h2>
         <br>
         <button class="button_reverse">Help in your community</button>
@@ -31,7 +31,7 @@ include("$homepath/includes/header.php");
         <img src="images/rewards.png" >
     </div>
     <div class="box-text">
-        <h2 style= "font-family: Trebuchet MS, Helvetica, sans-serif"> Helvetica, sans-serif;>You earned it! Redeem your volunteer rewards now</h2>
+        <h2 style= "font-family: Trebuchet MS, Helvetica, sans-serif"; font-size = "15px";>Redeem your volunteer rewards now. You earned it!</h2>
         <br><h2 style= "font-family: Trebuchet MS, Helvetica, sans-serif">• Restaurants • Local businesses • Entertainment</h2>
         <br>
 
