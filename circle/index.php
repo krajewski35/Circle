0c9b55efd139f941cc71314bca72f31f754aee7a
KunstmanBen <kunstm22@uwm.edu>
2023-10-31 12:23:08 -0500

    Update index.php

--- circle/index.php
+++ circle/index.php
@@ -26,9 +26,9 @@ include("$homepath/includes/header.php");
 </div>
 <div class="box">
     <div>
-        <h3>It's where sponsors can reward volunteers for their time.</h3>
+        <!-- <h3>It's where sponsors can reward volunteers for their time.</h3>
         <p>Volunteers receive points they can later exchange for rewards on Circle. Businesses large and small 
-            can provide them to connect with volunteers and promote their brand.</p>
+            can provide them to connect with volunteers and promote their brand.</p> -->
     <div class="box-text">
         <h3>Circle is where volunteering comes in full circle</h3>
     </div>
