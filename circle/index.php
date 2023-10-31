3b930685348f3803e8fb422f95c3b90bf469d3df
AichaAkbar <115574446+AichaAkbar@users.noreply.github.com>
2023-10-31 12:06:27 -0500

    Update index.php

--- circle/index.php
+++ circle/index.php
@@ -18,10 +18,7 @@ include("$homepath/includes/header.php");
 <div class="box">
     <div>
         <h2>Circle is where volunteering comes in full circle</h2>
-        <p>Circle is a one stop platform where volunteers and causes come together through an inutitive interface.
-            In return for the time and skills volunteers provide, they receive points they can later exchnage for 
-            sponsor provided rewards. This encourages volunteers to help, causes and individuals a convient way to
-            ask for help, and gives sponsors a way to promote their brand.</p>
+        
     </div>
     <div>
         <p>img here</p>
