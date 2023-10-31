6598c6a09dc3d8942ce35d9c03996ff5a96bf29d
KunstmanBen <kunstm22@uwm.edu>
2023-10-31 11:28:53 -0500

    box on home screen

--- circle/index.php
+++ circle/index.php
@@ -15,11 +15,19 @@ include("$homepath/includes/header.php");
 <div class= "content-wrap">
     <img src="images/mke.png" class="mke">
 </div>
-<h3>Circle is where volunteering comes in full circle</h3>
-<p>Circle is a one stop platform where volunteers and causes come together through an inutitive interface.
-    In return for the time and skills volunteers provide, they receive points they can later exchnage for 
-    sponsor provided rewards. This encourages volunteers to help, causes and individuals a convient way to
-    ask for help, and gives sponsors a way to promote their brand.</p>
+<div class="box">
+    <div>
+        <h3>Circle is where volunteering comes in full circle</h3>
+        <p>Circle is a one stop platform where volunteers and causes come together through an inutitive interface.
+            In return for the time and skills volunteers provide, they receive points they can later exchnage for 
+            sponsor provided rewards. This encourages volunteers to help, causes and individuals a convient way to
+            ask for help, and gives sponsors a way to promote their brand.</p>
+    </div>
+    <div>
+        <p>img here</p>
+    </div>
+</div>
+
 
 <?php
 //Include footer
