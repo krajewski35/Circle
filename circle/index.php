99517e10d10688e945c0d7cf9fc55ca5f71be7c9
KunstmanBen <kunstm22@uwm.edu>
2023-10-31 12:26:08 -0500

    Update index.php

--- circle/index.php
+++ circle/index.php
@@ -16,15 +16,6 @@ include("$homepath/includes/header.php");
     <img src="images/mke.png" class="mke">
 </div>
 <div class="box">
-    <div>
-        <h3>It's where causes receive the support they need</h3>
-        <p>Causes can easily post listings on Circle to get the help they need. No more difficult outreach!</p>
-    </div>
-    <div>
-        <p>img here</p>
-    </div>
-</div>
-<div class="box">
     <div class="box-text">
         <h3>Circle is where volunteering comes in full circle!</h3>
         <br>
