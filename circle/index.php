60e9e46c869cc42ed313244e8141f31cb7bdf0c3
krajewski35 <krajew35@uwm.edu>
2023-10-31 12:15:18 -0500

    Update index.php

--- circle/index.php
+++ circle/index.php
@@ -17,8 +17,18 @@ include("$homepath/includes/header.php");
 </div>
 <div class="box">
     <div>
-        <h2>Circle is where volunteering comes in full circle</h2>
-        
+        <h3>It's where causes receive the support they need</h3>
+        <p>Causes can easily post listings on Circle to get the help they need. No more difficult outreach!</p>
+    </div>
+    <div>
+        <p>img here</p>
+    </div>
+</div>
+<div class="box">
+    <div>
+        <h3>It's where sponsors can reward volunteers for their time.</h3>
+        <p>Volunteers receive points they can later exchange for rewards on Circle. Businesses large and small 
+            can provide them to connect with volunteers and promote their brand.</p>
     </div>
     <div>
         <p>img here</p>
