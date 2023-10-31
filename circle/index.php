5f8b45a98905fd6c8d30e64c6635f97e960a34f9
krajewski35 <krajew35@uwm.edu>
2023-10-31 08:36:16 -0500

    Update index.php

--- circle/index.php
+++ circle/index.php
@@ -12,25 +12,14 @@ $pagetitle = '';
 include("$homepath/includes/header.php");
 ?>
 
-<header>
-<head>
-    <link type="text/css" rel="stylesheet" href="indexstyle.css" />
-    <h3>Circle is where volunteering comes in full circle</h3>
-    <p>Circle is a one stop platform where volunteers and causes come together through an inutitive interface.
-        In return for the time and skills volunteers provide, they receive points they can later exchnage for 
-        sponsor provided rewards. This encourages volunteers to help, causes and individuals a convient way to
-        ask for help, and gives sponsors a way to promote their brand.</p>
-</head>
 <div class= "content-wrap">
     <img src="images/mke.png" class="mke">
 </div>
-</header>
-<ul>
-    <li><a class="active" href="login_page.php">Home</a></li>
-    <li><a href="login_page.php">Login</a></li>
-    <li><a href="login_page.php">Contact Us</a></li>
-    <li><a href="login_page.php">About Us</a></li>
-</ul>
+<h3>Circle is where volunteering comes in full circle</h3>
+<p>Circle is a one stop platform where volunteers and causes come together through an inutitive interface.
+    In return for the time and skills volunteers provide, they receive points they can later exchnage for 
+    sponsor provided rewards. This encourages volunteers to help, causes and individuals a convient way to
+    ask for help, and gives sponsors a way to promote their brand.</p>
 
 <?php
 //Include footer
