49948d42ee6a6f20bc36222072f04bfc6cf1977e
AichaAkbar <115574446+AichaAkbar@users.noreply.github.com>
2023-10-31 14:59:23 -0500

    Update index.php

--- circle/index.php
+++ circle/index.php
@@ -17,8 +17,8 @@ include("$homepath/includes/header.php");
 </div>
 <div class="box">
     <div class="box-text">
-        <h2>Circle is where volunteering comes in full circle!</h2>
-        <h2>• People • Causes • Rewards</h2>
+        <h2 style= "font-family: Trebuchet MS, Helvetica, sans-serif">Circle is where volunteering comes in full circle!</h2>
+        <h2 style= "font-family: Trebuchet MS, Helvetica, sans-serif">• People • Causes • Rewards</h2>
         <br>
         <button class="button_reverse">Help in your community</button>
     </div>
@@ -31,9 +31,8 @@ include("$homepath/includes/header.php");
         <img src="images/rewards.png" >
     </div>
     <div class="box-text">
-        <h2>You earned it!</h2>
-        <h2>Redeem your volunteer rewards now</h2>
-        <h2>• Restaurants • Local businesses • Entertainment</h2>
+        <h2 style= "font-family: Trebuchet MS, Helvetica, sans-serif"> Helvetica, sans-serif;>You earned it! Redeem your volunteer rewards now</h2>
+        <h2 style= "font-family: Trebuchet MS, Helvetica, sans-serif">• Restaurants • Local businesses • Entertainment</h2>
         <br>
 
         <button class="button_reverse">Help in your community</button>
