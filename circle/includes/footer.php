22b7f8468893c14c2c3e75d66c15fdaf5fae7c67
krajewski35 <krajew35@uwm.edu>
2023-11-02 14:04:25 -0500

    Footer & Flexbox

--- circle/includes/footer.php
+++ circle/includes/footer.php
@@ -1,7 +1,8 @@
         </div>
-        <div id = "footer">
+        <div class="footer">
             <hr>
-            <p><small><i>Circle Volunteering - 2023</i></small></p>
+            <p class="footer"><small><i>Circle Volunteering - 2023 | INFOST 490 - Senior Capstone</i></small></p>
+            <p class="footer"><small><i>Created by Patrick Tate, Matthew Yang, Lukas Krajewski, Ayesha Akbar, Ben Kunstman</i></small></p>
         </div>
     </body>
 </html>
