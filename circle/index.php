abcf626531e211c41cc214ead0ccb17aeca4ae16
krajewski35 <krajew35@uwm.edu>
2023-11-02 14:27:46 -0500

    Responisve Homepage Fix

--- circle/index.php
+++ circle/index.php
@@ -12,30 +12,32 @@ $pagetitle = 'Homepage';
 include("$homepath/includes/header.php");
 ?>
 
-<div class= "content-wrap">
+<div class="content-wrap">
     <img src="images/mke.png" class="mke"><br>
     <h2>Where volunteering comes in full circle!</h2>
     <a href=<?php echo "\"$urlpath/login/register_page.php\""; ?> ><button class="button">Join Circle</button></a>
 </div>
-<div class="box">
-    <div class="box-text">
-        <h2>Circle is a one-stop app for everyone!</h2>
-        <h2>• People • Causes • Sponsors •</h2>
-        <a href=<?php echo "\"$urlpath/nonexist.php\""; ?> ><button class="button-reverse">Help in your community</button></a>
+<div class="content-wrap">
+    <div class="box">
+        <div class="box-text">
+            <h2>Circle is a one-stop app for everyone!</h2>
+            <h2>• People • Causes • Sponsors •</h2>
+            <a href=<?php echo "\"$urlpath/nonexist.php\""; ?> ><button class="button-reverse">Help in your community</button></a>
+        </div>
+        <div class="box-img">
+            <img src="images/1.png" >
+        </div>
     </div>
-    <div class="box-img">
-        <img src="images/1.png" >
-    </div>
-</div>
-<div class="box">
-    <div class="box-img">
-        <img src="images/rewards.png" >
-    </div>
-    <div class="box-text">
-        <h2>Receive rewards for helping out!</h2>
-        <h2>• Restaurants • Local businesses • Entertainment •</h2>
+    <div class="box">
+        <div class="box-img">
+            <img src="images/rewards.png" >
+        </div>
+        <div class="box-text">
+            <h2>Receive rewards for helping out!</h2>
+            <h2>• Restaurants • Local businesses • Entertainment •</h2>
 
-       <a href=<?php echo "\"$urlpath/nonexist.php\""; ?> > <button class="button-reverse">Check out rewards</button></a>
+        <a href=<?php echo "\"$urlpath/nonexist.php\""; ?> > <button class="button-reverse">Check out rewards</button></a>
+        </div>
     </div>
 </div>
 
